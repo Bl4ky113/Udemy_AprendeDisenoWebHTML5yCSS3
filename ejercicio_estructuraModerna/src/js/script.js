9622745b6dd5819c709221f5a475f070418d449e
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-12-31 19:00:03 -0500

    Agregado Ejemplo section y article

--- ejercicio_estructuraModerna/src/js/script.js
+++ ejercicio_estructuraModerna/src/js/script.js
@@ -1,19 +1,38 @@
-/* ---- JS id,nameClass,tag shortcuts ---- */
+/* ---- JS DOM shortcuts ---- */
 
 const ById = document.getElementById.bind(document);
+const ByClass = document.getElementsByClassName.bind(document);
+
+const log = console.log;
 
 /* Change of side the ".main-nav" */
 
+//Get the elements
 let nav = ById("nav");
+let section = ByClass("main-section");
 
+//init the float value of the elements
 nav.style.float = "right";
 
+//Change the elements float value
 function changeSide () {
     if (nav.style.float == "right") {
         nav.style.float = "left";
+        setClass_float(section, "float", "right", 3);
     }
     else {
         nav.style.float = "right";
+        setClass_float(section, "float", "left", 3);
     }
+}
 
+for (let i = 0; i < section.length; i++) {
+    section[i].style.float = "left";
+}
+//Change Elements floats value
+function setClass_float (className, setStyle, value, lenght) {
+    for (let i = 0; i < lenght; i++) {
+        className[i].style.float = value;
+        console.log(i, section[i].style); 
+    }
 }
