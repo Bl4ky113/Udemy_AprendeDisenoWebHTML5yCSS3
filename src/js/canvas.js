9d80f760adfd6e372118a44e0cdef97eec949135
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-05-10 17:37:10 -0500

    Arreglado ejemplo canvasRectangulosColores y whitespace en design responsive

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -11,18 +11,18 @@ let canvasComplejo = document.getElementById("canvas-complejo").getContext("2d")
 // Rectangulos Rojos pequeños
 canvasComplejo.fillStyle = "rgba(255, 0, 0, 0.4)";
 canvasComplejo.fillRect(0, 0, 30, 30);
-canvasComplejo.fillRect(270, 0, 30, 30);
-canvasComplejo.fillRect(0, 270, 30, 30);
+canvasComplejo.fillRect(170, 0, 30, 30);
+canvasComplejo.fillRect(0, 170, 30, 30);
 //Rectangulo Rojo Grande
-canvasComplejo.fillRect(30, 30, 150, 150)
+canvasComplejo.fillRect(30, 30, 100, 100)
 
 //Rectangulos Azules Pequeños
 canvasComplejo.fillStyle = "rgba(0, 0, 255, 0.4)";
-canvasComplejo.fillRect(270, 270, 30, 30);
-canvasComplejo.fillRect(270, 0, 30, 30);
-canvasComplejo.fillRect(0, 270, 30, 30);
+canvasComplejo.fillRect(170, 170, 30, 30);
+canvasComplejo.fillRect(170, 0, 30, 30);
+canvasComplejo.fillRect(0, 170, 30, 30);
 //Rectangulo Azul Grande
-canvasComplejo.fillRect(120, 120, 150, 150);
+canvasComplejo.fillRect(70, 70, 100, 100);
 
 /* -- Ejemplo de Figuras en el Canvas -- */
 
